3df6845cc00fbb0006a5b2c7440cb30f03314131
ashleighhh <ashleighmudge.student@wegc.school.nz>
2015-05-28 09:02:42 +1200

    Create auction.py
    Version without any verification of input

---
+++ auction.py
@@ -0,0 +1,33 @@
+#The beginning
+reserve_price = int(input("What is the reserve price?"))
+highest_bid = 0
+name = ""
+bid = 0
+names = [""]
+bids = [0]
+#The auction
+print ("Auction has started")
+while name.upper() != 'F' :
+    print ("Highest bid so far is $"+ str(max(bids))+ " \"F\" to finish)")
+    name = input("What is your name?")
+    name = name.title()
+    print (name)
+    if name.upper()!= "F":
+        bid = int(input("What is your bid?"))
+        print (bid)
+        if bid > highest_bid:
+            names.append(name)
+            bids.append(bid)
+            highest_bid = bid
+        else:
+            print ("Sorry ", name, " You'll need to make another, higher bid.")
+
+#The end
+if highest_bid >= reserve_price:
+    print ("Auction won by ", names[len(names)-1], " with a bid of $", highest_bid)
+    for i in range (0, len(names)):
+        print (names[i], " bid ", bids[i])
+else:
+    print ("Auction did not meet reserve price")
+    for i in range (0, len(names)):
+        print (names[i], " bid ", bids[i])
